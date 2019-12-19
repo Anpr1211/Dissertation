3b1d51420d51179d5a2754e7a509666671c53bf2
Ankita Prakash <37382022+Anpr1211@users.noreply.github.com>
2019-12-20 02:43:03 +0530

    Added comments

--- tsdlDataViz.R
+++ tsdlDataViz.R
@@ -1,9 +1,15 @@
+# importing libraries
+
 library(shiny)
 library(shinydashboard)
 library(tsdl)
 library(forecast)
 
+# list of subjects covered by the tsdl library
+
 subject = sort(unique(meta_tsdl$subject))
+
+# function to get the titles of the time series of the chosen category
 
 get_title = function(category){
   
@@ -18,12 +24,16 @@ get_title = function(category){
   return(title_list)
 }
 
+# function to get the source of the chosen time series
+
 get_source = function(title){
   
   data <- subset(tsdl, description=title)
   source = attributes(data[[1]])$source
   return(source)
 }
+
+# function to get the start date of the chosen time series
 
 get_startdt = function(title){
   
@@ -32,12 +42,16 @@ get_startdt = function(title){
   return(startdt)
 }
 
+# function to get the end date of the chosen time series
+
 get_enddt = function(title){
   
   data <- subset(tsdl, description=title)
   enddt = attributes(data[[1]])$tsp[2]
   return(enddt)
 }
+
+# function to get the frequency of the chosen time series
 
 get_freq = function(title){
   
@@ -46,23 +60,29 @@ get_freq = function(title){
   return(freq)
 }
 
+# function to get data of the chosen time series
+
 get_data = function(title){
   
   data <- subset(tsdl, description=title)
   return(data[[1]])
 }
 
+# user interface of the web app
+
 ui <-fluidPage(
   titlePanel("tsdl library - Data Visualizations"),
   fluidRow(
     column(3, wellPanel(
+      # This outputs the dynamic UI component - the list of subjects
       selectInput("subject", " Subject", subject)
     )),
     column(9, wellPanel(
-      # This outputs the dynamic UI component
+      # This outputs the dynamic UI component - the list of titles of the time series of the chosen subjects
       uiOutput("ui")
     )),
-    column(3, 
+    # This outputs the dynamic UI component - the details of the time series
+    column(3,
            tags$h2("Stuff you might want to know -")
     ),
     column(4,
@@ -78,11 +98,12 @@ ui <-fluidPage(
            verbatimTextOutput("enddt")
            
            ),
+   # This outputs the dynamic UI component - the plot of the time series
   plotOutput("plot")
 )
 )
 
-
+# the seerver function
 
 server <- function(input, output){
   
